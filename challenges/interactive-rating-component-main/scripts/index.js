b4748c35ed527dd4863f7b9b9eb6dc4aa8b2660b
Jessie Jalca <jessiejalca@gmail.com>
2023-01-02 22:46:11 -0500

    feat: Change image after submission TASK Updated component image to more appropriate "thank you" image for better feedback. NOTE Also edited selection number notice for better grammar.

--- challenges/interactive-rating-component-main/scripts/index.js
+++ challenges/interactive-rating-component-main/scripts/index.js
@@ -14,22 +14,24 @@ const fetchRating = () => {
   for (i = 0; i < RATING_OPTIONS.length; i++) {
     if (RATING_OPTIONS[i].checked) {
       userRating = RATING_OPTIONS[i].value;
+      return userRating;
     }
   }
-
-  return userRating;
 };
 
 const postResult = (userRating) => {
+  const IMG = COMPONENT.querySelector("img");
   const TITLE = COMPONENT.querySelector("h1");
   const SUB = COMPONENT.querySelector("p");
   const RESULT = document.createElement("p");
 
+  IMG.setAttribute("src", "./images/illustration-thank-you.svg");
+  IMG.setAttribute("alt", "abstract device illustration");
   TITLE.textContent = `Thank you!`;
   TITLE.before(RESULT);
   SUB.textContent = `We appreciate you taking the time to give a rating.
   If you ever need more support, don't hesitate to get in touch!`;
   RESULT.className = "result";
-  RESULT.textContent = `You select ${userRating} out of 5`;
+  RESULT.textContent = `You selected ${userRating} out of 5`;
   FORM.remove();
 };
