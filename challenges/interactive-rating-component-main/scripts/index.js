f7898a75b72a7bba59537c67cbf29704fba5b260
Jessie Jalca <jessiejalca@gmail.com>
2023-01-02 22:09:47 -0500

    feat: Post results and thank user after submission TASK Provided feedback for the user after submitting the form. Included the chosen rating, removed the form, and changed the message to be an appropriate "thank you" message. CHANGES Moved rating retrieval into separate function and called it as an input for the new postResults() function. Deleted some comments meant for feature reference in index.html.

--- challenges/interactive-rating-component-main/scripts/index.js
+++ challenges/interactive-rating-component-main/scripts/index.js
@@ -1,15 +1,35 @@
+const COMPONENT = document.getElementById("rating-component");
 const FORM = document.getElementById("rating-form");
 const SUBMIT = document.getElementById("submit");
 
 SUBMIT.addEventListener("click", (e) => {
+  e.preventDefault();
+  postResult(fetchRating());
+});
+
+const fetchRating = () => {
   const RATING_OPTIONS = document.getElementsByName("rating");
   let userRating;
 
-  e.preventDefault();
-
   for (i = 0; i < RATING_OPTIONS.length; i++) {
-    if (RATING_OPTIONS[i].checked) userRating = RATING_OPTIONS[i].value;
+    if (RATING_OPTIONS[i].checked) {
+      userRating = RATING_OPTIONS[i].value;
+    }
   }
 
-  console.log(userRating);
-});
+  return userRating;
+};
+
+const postResult = (userRating) => {
+  const TITLE = COMPONENT.querySelector("h1");
+  const SUB = COMPONENT.querySelector("p");
+  const RESULT = document.createElement("p");
+
+  TITLE.textContent = `Thank you!`;
+  TITLE.before(RESULT);
+  SUB.textContent = `We appreciate you taking the time to give a rating.
+  If you ever need more support, don't hesitate to get in touch!`;
+  RESULT.className = "result";
+  RESULT.textContent = `You select ${userRating} out of 5`;
+  FORM.remove();
+};
